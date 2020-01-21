9ae4b42228dcb880e556eeba6e74ef52518baf2d
jstiven01 <stiven10@gmail.com>
2020-01-21 18:55:57 -0500

    Styling Button and adding default props to color and wide

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -4,34 +4,34 @@ import Button from './Button';
 function ButtonPanel() {
   return (
     <div>
-      <div id="group-1">
-        <Button buttonName="AC" />
-        <Button buttonName="+/-" />
-        <Button buttonName="%" />
+      <div className="row-buttons" id="group-1">
+        <Button buttonName="AC" color="gray" />
+        <Button buttonName="+/-" color="gray" />
+        <Button buttonName="%" color="gray" />
         <Button buttonName="÷" />
       </div>
-      <div id="group-2">
-        <Button buttonName="7" />
-        <Button buttonName="8" />
-        <Button buttonName="9" />
+      <div className="row-buttons" id="group-2">
+        <Button buttonName="7" color="gray" />
+        <Button buttonName="8" color="gray" />
+        <Button buttonName="9" color="gray" />
         <Button buttonName="X" />
       </div>
-      <div id="group-3">
-        <Button buttonName="4" />
-        <Button buttonName="5" />
-        <Button buttonName="6" />
+      <div className="row-buttons" id="group-3">
+        <Button buttonName="4" color="gray" />
+        <Button buttonName="5" color="gray" />
+        <Button buttonName="6" color="gray" />
         <Button buttonName="-" />
 
       </div>
-      <div id="group-4">
-        <Button buttonName="1" />
-        <Button buttonName="2" />
-        <Button buttonName="3" />
+      <div className="row-buttons" id="group-4">
+        <Button buttonName="1" color="gray" />
+        <Button buttonName="2" color="gray" />
+        <Button buttonName="3" color="gray" />
         <Button buttonName="+" />
       </div>
-      <div id="group-5">
-        <Button buttonName="0" />
-        <Button buttonName="." />
+      <div className="row-buttons" id="group-5">
+        <Button buttonName="0" color="gray" wide />
+        <Button buttonName="." color="gray" />
         <Button buttonName="=" />
       </div>
     </div>
